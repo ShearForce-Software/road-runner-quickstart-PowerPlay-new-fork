0a4fbb0cc35d1977cae2f32a9f2686ead8d5938b
mtroesser <mtroesser@live.com>
2022-10-25 20:31:57 -0500

    Calibration Day 1 - Dev Bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Auto_Route_Left_Far.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Auto_Route_Left_Far.java
@@ -3,13 +3,13 @@ package org.firstinspires.ftc.teamcode.drive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 /*
  * This is an example of a more complex path to really test the tuning.
  */
-@Autonomous(group = "drive")
+@TeleOp(name = "Auto Route")
 public class Auto_Route_Left_Far extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -22,7 +22,7 @@ public class Auto_Route_Left_Far extends LinearOpMode {
         //sleep(2000);
 
         drive.followTrajectorySequence(
-                drive.trajectorySequenceBuilder(new Pose2d(-36, 60, Math.toRadians(-90)))
+                drive.trajectorySequenceBuilder(new Pose2d(-36, 36, Math.toRadians(-90)))
                         .splineToLinearHeading(new Pose2d(-50, 12, Math.toRadians(0)), Math.toRadians(-180))
                         //1st cone
                         .splineToLinearHeading(new Pose2d(-31, 7, Math.toRadians(-45)), Math.toRadians(-45))
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -116,10 +116,10 @@ public class SampleMecanumDrive extends MecanumDrive {
         // For example, if +Y in this diagram faces downwards, you would use AxisDirection.NEG_Y.
         // BNO055IMUUtil.remapZAxis(imu, AxisDirection.NEG_Y);
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "left_front");
+        leftRear = hardwareMap.get(DcMotorEx.class, "left_rear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "right_rear");
+        rightFront = hardwareMap.get(DcMotorEx.class, "right_front");
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
@@ -140,9 +140,12 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightRear.setDirection(DcMotor.Direction.REVERSE);
 
         // TODO: if desired, use setLocalizer() to change the localization method
         // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
+        setLocalizer(new StandardTrackingWheelLocalizer(hardwareMap));
 
         trajectorySequenceRunner = new TrajectorySequenceRunner(follower, HEADING_PID);
     }

8fe8c21eac3af10c9c46e493bfe2bf1a4abe184a
Jacob <jacoblemanski@stdominichs.org>
2022-11-03 20:30:31 -0500

    slide different levels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SlideMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SlideMotors.java
@@ -11,9 +11,10 @@ public class SlideMotors extends LinearOpMode {
     DcMotor slideOne;
     DcMotor slideTwo;
 
+    int desiredPos = 0;
+    int runToPos = 0;
 
-    public static final double ARM_UP_POWER    =  0.50 ;
-    public static final double ARM_DOWN_POWER  = 0.50 ;
+    public static final double ARM_POWER    =  0.50 ;
 
     public void runOpMode() {
         slideOne = hardwareMap.get(DcMotor.class, "slideOne");
@@ -26,16 +27,26 @@ public class SlideMotors extends LinearOpMode {
         telemetry.update();
         waitForStart();
         while (opModeIsActive()) {
+            slideOne.setPower(ARM_POWER);
+            slideTwo.setPower(ARM_POWER);
             if (gamepad1.dpad_up) {
-                slideOne.setPower(ARM_UP_POWER);
-                slideTwo.setPower(ARM_UP_POWER);
-            }else if (gamepad1.dpad_down) {
-                slideOne.setPower(ARM_DOWN_POWER);
-                slideTwo.setPower(ARM_DOWN_POWER);
-            }else {
-                slideOne.setPower(0.0);
-                slideTwo.setPower(0.0);
+                desiredPos = 1612;
+                runToPos = desiredPos - slideOne.getCurrentPosition();
+                slideOne.setTargetPosition(runToPos);
+                slideTwo.setTargetPosition(runToPos);
+            }else if (gamepad1.dpad_left) {
+                desiredPos = 1075;
+                runToPos = desiredPos - slideOne.getCurrentPosition();
+                slideOne.setTargetPosition(runToPos);
+                slideTwo.setTargetPosition(runToPos);
+            }else if (gamepad1.dpad_down){
+                desiredPos = 0;
+                runToPos -= slideOne.getCurrentPosition();
+                slideOne.setTargetPosition(runToPos);
+                slideTwo.setTargetPosition(runToPos);
+
             }
+
 
         }
     }

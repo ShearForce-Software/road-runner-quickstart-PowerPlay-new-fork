62c915b0f36c344fa31deef563756bcfdd668c5d
xavier <xavierzimmerman22@gmail.com>
2022-11-13 18:17:59 -0600

    started final autounomous routes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Auto_Route_Left_Far.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Auto_Route_Left_Far.java
@@ -4,15 +4,21 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /*
  * This is an example of a more complex path to really test the tuning.
  */
 @TeleOp(name = "Auto Route Left Far")
 public class Auto_Route_Left_Far extends LinearOpMode {
+    private CustomModelWebcam customModelWebcam = new CustomModelWebcam();
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
+        customModelWebcam.initWebcam();
+        customModelWebcam.runOpModeActive();
+        String sleeveLabel = customModelWebcam.label;
+        AtomicInteger parkingSpot = new AtomicInteger();
         waitForStart();
 
         if (isStopRequested()) return;
@@ -21,6 +27,16 @@ public class Auto_Route_Left_Far extends LinearOpMode {
 
         drive.followTrajectorySequence(
                 drive.trajectorySequenceBuilder(new Pose2d(36, 36, Math.toRadians(-90)))
+                        .addDisplacementMarker(() -> {
+                            if (sleeveLabel.equals("Checkered1")) {
+                                parkingSpot.set(1);
+                            }
+                            else if (sleeveLabel.equals("Logo3")){
+                                parkingSpot.set(3);
+                            }else{
+                                parkingSpot.set(2);
+                            }
+        })
                         .splineToLinearHeading(new Pose2d(50, 12, Math.toRadians(-180)), Math.toRadians(0))
                         //1st cone
                         .splineToLinearHeading(new Pose2d(31, 7, Math.toRadians(135)), Math.toRadians(135))
@@ -59,6 +75,9 @@ public class Auto_Route_Left_Far extends LinearOpMode {
                         .setReversed(false)
                         //6th cone
                         .splineToLinearHeading(new Pose2d(31, 7, Math.toRadians(135)), Math.toRadians(135))
+                        .addDisplacementMarker(() -> {
+                            //ADD algorithm to make robot go to parking spot based off of parkingSpot int
+                        })
                         .build()
         );
     }

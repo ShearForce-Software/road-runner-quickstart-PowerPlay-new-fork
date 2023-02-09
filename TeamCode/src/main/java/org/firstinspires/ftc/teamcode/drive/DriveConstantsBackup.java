feab2ef98e7c908fc68497522e654e1f00ada756
DanTroesser <dan.troesser@live.com>
2023-02-08 20:41:09 -0600

    Removing conflicts so we can sync with upstream acmerobotics/road-runner-quickstart updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstantsBackup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstantsBackup.java
@@ -17,7 +17,7 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
  * and op modes themselves.
  */
 @Config
-public class DriveConstants {
+public class DriveConstantsBackup {
 
     /*
      * These are motor constants that should be listed online for your motors.
@@ -33,7 +33,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
+    public static final boolean RUN_USING_ENCODER = false;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
       50.5);
 
